69b7c34fa932e2fefd9d8ed0ddfde66d77c07d82
mauriciooliver <mauriciooliver@gmail.com>
2023-12-13 22:44:24 -0400

    removendo_calculo_naodiponivelV1

--- termometria2.0/query_database.py
+++ termometria2.0/query_database.py
@@ -1,6 +1,6 @@
 
 def get_temperatura():
-    return "SELECT * FROM registro_instalacao WHERE  codigo = 2910;"
+    return "SELECT * FROM registro_instalacao WHERE integracao is null order by codigo desc limit 1;"
 
 def get_cliente():
     return "SELECT ConfigInstalacao.id_cliente, ConfigInstalacao.id_planta, ConfigInstalacao.id_equipamento, ConfigInstalacao.id_placa FROM ConfigInstalacao;" 
@@ -35,3 +35,5 @@ def error_status500():
 
 def failure_record_integration():
     return "INSERT INTO log_erros (TIPO_ERRO, MSG_ERRO , ID_ERRO) VALUES ('Falha ao enviar dados termometria', 'FALHA: essa menssagem é para quando o endpoint retorna um Json vazio indicando uma possivel falha , proxima tentativa em 1 minuto!', 8)"
+def error_except_exception():
+    return "INSERT INTO log_erros (TIPO_ERRO, MSG_ERRO , ID_ERRO) VALUES ('Falha ao enviar dados termometria', 'FALHA: essa menssagem é para quando o endpoint retorna um Json vazio indicando uma possivel falha , proxima tentativa em 1 minuto!', 9)"
